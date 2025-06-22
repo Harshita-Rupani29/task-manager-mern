a609ac41a218fdfc48fc0fffec601af7b77ce1bc
Harshita <harshitarupani4@gmail.com>
2025-06-22 16:41:51 +0530

    DEPLOYING FRONTEND

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/tasks";
+const API_URL = "https://task-manager-mern-f18l.onrender.com/tasks";
 
 export const getTasks = () => axios.get(API_URL);
 export const createTask = (data) => axios.post(API_URL, data);
